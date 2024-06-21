8b417dd08d5f719c66368c98bfc067b1ef640ab8
sonsuchang <son336208@naver.com>
2024-06-21 20:42:42 +0900

    UserEntity 내부 값 하나씩 불러오기

--- src/main/java/com/example/feple/Controller/UserController.java
+++ src/main/java/com/example/feple/Controller/UserController.java
@@ -37,14 +37,14 @@ public class UserController {
         userRepository.save(userEntity);
     }
 
-    @GetMapping("/followartist/{uid}")
-    public ResponseEntity<List<String>> getFollowArtistByUid(@PathVariable String uid) {
+    @GetMapping("/followartist")
+    public ResponseEntity<List<String>> getFollowArtistByUid(@RequestParam String uid) {
         UserEntity userEntity = userRepository.findByUid(uid);
         return ResponseEntity.ok(userEntity.getFollow_artist());
     }
 
-    @PatchMapping("/followartist/{uid}")
-    public ResponseEntity<String> updateFollowArtistByUid(@PathVariable String uid, @RequestBody String ArtistName){
+    @PatchMapping("/followartist")
+    public ResponseEntity<String> updateFollowArtistByUid(@RequestParam String uid, @RequestBody String ArtistName){
         UserEntity userEntity = userRepository.findByUid(uid);
         List<String> newFollowArtist = userEntity.getFollow_artist();
         newFollowArtist.add(ArtistName);
@@ -53,70 +53,70 @@ public class UserController {
         return ResponseEntity.ok("FollowArtist updated successfully");
     }
 
-    @GetMapping("/nickname/{uid}")
-    public ResponseEntity<String> getNicknameByUid(@PathVariable String uid) {
+    @GetMapping("/nickname")
+    public ResponseEntity<String> getNicknameByUid(@RequestParam String uid) {
         UserEntity userEntity = userRepository.findByUid(uid);
         return ResponseEntity.ok(userEntity.getNickname());
     }
 
-    @PatchMapping("/nickname/{uid}")
-    public ResponseEntity<String> updateNicknameByUid(@PathVariable String uid, @RequestBody String newNickname) {
+    @PatchMapping("/nickname")
+    public ResponseEntity<String> updateNicknameByUid(@RequestParam String uid, @RequestBody String newNickname) {
         UserEntity userEntity = userRepository.findByUid(uid);
         userEntity.setNickname(newNickname);
         userRepository.save(userEntity);
         return ResponseEntity.ok("Nickname updated successfully");
     }
 
-    @GetMapping("/postnum/{uid}")
-    public ResponseEntity<Long> getPostnumByUid(@PathVariable String uid){
+    @GetMapping("/postnum")
+    public ResponseEntity<Long> getPostnumByUid(@RequestParam String uid){
         UserEntity userEntity = userRepository.findByUid(uid);
         return ResponseEntity.ok(userEntity.getPost_num());
     }
 
-    @PatchMapping("/postnum/{uid}")
-    public ResponseEntity<String> updatePostnumByUid(@PathVariable String uid){
+    @PatchMapping("/postnum")
+    public ResponseEntity<String> updatePostnumByUid(@RequestParam String uid){
         UserEntity userEntity = userRepository.findByUid(uid);
         Long newPost_num = userEntity.getPost_num() + 1;
         userEntity.setPost_num(newPost_num);
         return ResponseEntity.ok("Postnum updated successfully");
     }
 
-    @GetMapping("/commentnum/{uid}")
-    public ResponseEntity<Long> getCommentnumByUid(@PathVariable String uid){
+    @GetMapping("/commentnum")
+    public ResponseEntity<Long> getCommentnumByUid(@RequestParam String uid){
         UserEntity userEntity = userRepository.findByUid(uid);
         return ResponseEntity.ok(userEntity.getComment_num());
     }
 
-    @PatchMapping("/commentnum/{uid}")
-    public ResponseEntity<String> updateCommentnumByUid(@PathVariable String uid){
+    @PatchMapping("/commentnum")
+    public ResponseEntity<String> updateCommentnumByUid(@RequestParam String uid){
         UserEntity userEntity = userRepository.findByUid(uid);
         Long newComment_num = userEntity.getComment_num() + 1;
         userEntity.setComment_num(newComment_num);
         return ResponseEntity.ok("Commentnum updated successfully");
     }
 
-    @GetMapping("/bookmarknum/{uid}")
-    public ResponseEntity<Long> getBookmarknumByUid(@PathVariable String uid){
+    @GetMapping("/bookmarknum")
+    public ResponseEntity<Long> getBookmarknumByUid(@RequestParam String uid){
         UserEntity userEntity = userRepository.findByUid(uid);
         return ResponseEntity.ok(userEntity.getBookmark_num());
     }
 
-    @PatchMapping("/bookmarknum/{uid}")
-    public ResponseEntity<String> updateBookmarknumByUid(@PathVariable String uid){
+    @PatchMapping("/bookmarknum")
+    public ResponseEntity<String> updateBookmarknumByUid(@RequestParam String uid){
         UserEntity userEntity = userRepository.findByUid(uid);
         Long newBookmark_num = userEntity.getBookmark_num() + 1;
         userEntity.setBookmark_num(newBookmark_num);
         return ResponseEntity.ok("Bookmarknum updated successfully");
     }
 
-    @GetMapping("/level/{uid}")
-    public ResponseEntity<Long> getLevelByUid(@PathVariable String uid){
+    @GetMapping("/level")
+    public ResponseEntity<Long> getLevelByUid(@RequestParam String uid){
         UserEntity userEntity = userRepository.findByUid(uid);
         return ResponseEntity.ok(userEntity.getLevel());
     }
 
-    @PatchMapping("/level/{uid}")
-    public ResponseEntity<String> updateLevelByUid(@PathVariable String uid){
+    @PatchMapping("/level")
+    public ResponseEntity<String> updateLevelByUid(@RequestParam String uid){
         UserEntity userEntity = userRepository.findByUid(uid);
         Long newLevel = userEntity.getLevel(); //TODO Level 업데이트 어떻게 해야할지 생각해야함
         userEntity.setPost_num(newLevel);
