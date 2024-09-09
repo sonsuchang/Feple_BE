8590294c98f58dfa68d961ca7b04cca24287d6b7
sonsuchang <son336208@naver.com>
2024-09-09 11:45:39 +0900

    아티스트 페스티벌 GET

--- src/main/java/com/example/feple/Controller/ArtistController.java
+++ src/main/java/com/example/feple/Controller/ArtistController.java
@@ -3,10 +3,10 @@ package com.example.feple.Controller;
 import com.example.feple.Entity.ArtistEntity;
 import com.example.feple.Repository.ArtistRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,5 +18,15 @@ public class ArtistController {
     @PostMapping("/save")
     public void saveArtist(@RequestBody ArtistEntity artistEntity) {
         artistRepository.save(artistEntity);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<ArtistEntity>> getAllArtist(){
+        List<ArtistEntity> artists = artistRepository.findAll();
+        if (artists.isEmpty()){
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.ok(artists);
+        }
     }
 }
--- src/main/java/com/example/feple/Controller/FestivalController.java
+++ src/main/java/com/example/feple/Controller/FestivalController.java
@@ -3,19 +3,30 @@ package com.example.feple.Controller;
 import com.example.feple.Entity.FestivalEntity;
 import com.example.feple.Repository.FestivalRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/festival")
+@RequestMapping("/festival")
 public class FestivalController {
+
     private final FestivalRepository festivalRepository;
 
     @PostMapping("/save")
     public void saveFestival(@RequestBody FestivalEntity festivalEntity) {
         festivalRepository.save(festivalEntity);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<FestivalEntity>> getAllFestival(){
+        List<FestivalEntity> festivals = festivalRepository.findAll();
+        if (festivals.isEmpty()){
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.ok(festivals);
+        }
     }
 }
